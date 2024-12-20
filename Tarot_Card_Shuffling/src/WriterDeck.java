9f0b9f13468b43bd87f23211155f7e8b60333a51
Serena <168666459+krueg586@users.noreply.github.com>
2024-12-19 19:16:07 -0600

    Create WriterDeck.java

---
+++ Tarot_Card_Shuffling/src/WriterDeck.java
@@ -0,0 +1,33 @@
+
+public class WriterDeck {
+	 public static Writer[] initialize() {
+	        Writer[] writers = new Writer[26];
+	        writers[0] = new Writer("writer1", new int[]{});
+			writers[1] = new Writer("writer2", new int[]{});
+			writers[2] = new Writer("writer3", new int[]{4, 7, 27, 31, 35});
+			writers[3] = new Writer("writer4", new int[]{23, 29, 30, 33, 72});
+			writers[4] = new Writer("writer5", new int[]{28, 35, 40, 55, 56});
+			writers[5] = new Writer("writer6", new int[]{23, 28, 30, 49, 77});
+			writers[6] = new Writer("writer7", new int[]{23, 43, 54, 55, 58});
+			writers[7] = new Writer("writer8", new int[]{17, 28, 39, 53});
+			writers[8] = new Writer("writer9", new int[]{});
+			writers[9] = new Writer("writer10", new int[]{26, 30, 47, 51, 53});
+			writers[10] = new Writer("writer11", new int[]{17, 23, 35, 40, 57});
+			writers[11] = new Writer("writer12", new int[]{3, 23, 30, 32, 49});
+			writers[12] = new Writer("writer13", new int[]{7, 32, 35, 49, 53});
+			writers[13] = new Writer("writer14", new int[]{23, 25, 43, 53, 67});
+			writers[14] = new Writer("writer15", new int[]{12, 17, 19, 37, 47});
+			writers[15] = new Writer("writer16", new int[]{20, 27, 30, 63, 77});
+			writers[16] = new Writer("writer17", new int[]{4, 35, 43, 53, 77});
+			writers[17] = new Writer("writer18", new int[]{23, 24, 37, 39, 43});
+			writers[18] = new Writer("writer19", new int[]{23, 34, 39, 40, 43});
+			writers[19] = new Writer("writer20", new int[]{20, 23, 43, 49, 77});
+			writers[20] = new Writer("writer21", new int[]{5, 34, 39, 53, 58});
+			writers[21] = new Writer("writer22", new int[]{7, 17, 50, 55});
+			writers[22] = new Writer("writer23", new int[]{26, 34, 35, 49, 54});
+			writers[23] = new Writer("writer24", new int[]{10, 25, 31, 46, 71});
+			writers[24] = new Writer("writer25", new int[]{9, 15, 45, 56, 70});
+			writers[25] = new Writer("writer26", new int[]{25, 28, 32, 57, 67});
+	        return writers;
+	    }
+}
